3f333ffc1794e77b3e28a2758e46f393161a30b3
Taymur-rex <taymur.dixon@hgs.hiddengeniusproject.org>
2026-07-16 14:53:28 -0700

    Player Rotation
    Updated the player controller to rotate the player with the mouse

--- Hack/Assets/Scripts/PlayerControlleer.cs
+++ Hack/Assets/Scripts/PlayerControlleer.cs
@@ -1,84 +1,102 @@
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 public class PlayerControlleer : MonoBehaviour
 {
-   // Store the input action sheet used for input 
-   [SerializeField] private InputActionAsset InputActions; 
+    // Store the input action sheet used for input
+    [SerializeField] private InputActionAsset InputActions;
 
-   //ACTIONS 
-   private InputAction moveAction;
-   private InputAction jumpAction;
+    // ACTIONS
+    private InputAction moveAction;
+    private InputAction jumpAction;
 
-   private Vector2 moveInput;
-   [SerializeField] private LayerMask groundLayer;
-   [SerializeField] private float groundCheckDistance = 1f;
+    private Vector2 moveInput;
 
-   // COMPONENTS
-   [SerializeField] private Rigidbody rb; 
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundCheckDistance = 1f;
 
-   // PLAYER SETTINGS 
-   [SerializeField] private float moveSpeed = 5f;
+    // NEW - Mouse Aim
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private LayerMask aimLayer;
 
-   [SerializeField] private float jumpForce = 5f; 
+    // COMPONENTS
+    [SerializeField] private Rigidbody rb;
 
-   // Awake() is called once only 
-   private void Awake()
-   {
-      moveAction = InputSystem.actions.FindAction("Move");
-      jumpAction = InputSystem.actions.FindAction("Jump");
+    // PLAYER SETTINGS
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
 
-      rb = GetComponent<Rigidbody>();
-   }
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
 
-   private void OnEnable()
-   {
-      InputActions.FindActionMap("Player").Enable();
-   }
+        rb = GetComponent<Rigidbody>();
+    }
 
-   private void OnDisable()
-   {
-      InputActions.FindAction("Player")?.Disable();
-   }
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
 
-   private void Update() 
-   {
-      moveInput = moveAction.ReadValue<UnityEngine.Vector2>(); 
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player")?.Disable();
+    }
 
-      if (jumpAction.WasPressedThisFrame())
-      {
-         HandleJump();
-      }
-   }
+    private void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
 
-    private void FixedUpdate() 
-   {
-      HandleMovement();
-   }
+        HandleRotation();
 
-   private void HandleMovement()
-   {
-      UnityEngine.Vector3 moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x; 
+        if (jumpAction.WasPressedThisFrame())
+        {
+            HandleJump();
+        }
+    }
 
-      moveDirection.Normalize();
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
 
-      rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.deltaTime); 
-   }
+    private void HandleMovement()
+    {
+        Vector3 moveDirection = transform.forward * moveInput.y +
+                                transform.right * moveInput.x;
 
-   private void HandleJump()
-   {
-      if (IsGrounded())
-      {
-        rb.AddForce(UnityEngine.Vector3.up * jumpForce, ForceMode.Impulse);
-      }
-   }
+        moveDirection.Normalize();
 
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.deltaTime);
+    }
 
-  private bool IsGrounded()
-  {
-    return Physics.Raycast(transform.position, UnityEngine.Vector3.down, groundCheckDistance, groundLayer);
-  }
+    private void HandleRotation()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, aimLayer))
+        {
+            Vector3 lookDirection = hit.point - transform.position;
+            lookDirection.y = 0f;
+
+            if (lookDirection.sqrMagnitude > 0.001f)
+            {
+                transform.rotation = Quaternion.LookRotation(lookDirection);
+            }
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
+    }
 }
-
-
